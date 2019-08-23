68dee04cfcea18fcea31df13615e66155f765a05
longniao <longniao@gmail.com>
2019-08-23 19:24:52 +0800

    version 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 from setuptools import setup, find_packages
 from collections import OrderedDict
 
-version = "0.0.1"
+version = "0.0.2"
 
 readme = ''
 with io.open('README.rst', 'rt', encoding='utf8') as f:
